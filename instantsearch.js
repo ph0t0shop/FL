ab96bffb30b7547565c0ed00ae1e8481084ef346
ph0t0shop <lankreijersten@gmail.com>
2023-02-24 17:19:44 +0100

    First commitd

--- instantsearch.js
+++ instantsearch.js
@@ -89,6 +89,7 @@ requirejs(['algoliaBundle', 'Magento_Catalog/js/price-utils', 'jquery/jquery.coo
             searchClient: searchClient,
             indexName: indexName,
             searchFunction: function (helperBypass) {
+                alert("yay");
                 helperBypass.search();
                 $('.algolia-instant-replaced-content').hide();
                 $('.algolia-instant-selector-results').show();
